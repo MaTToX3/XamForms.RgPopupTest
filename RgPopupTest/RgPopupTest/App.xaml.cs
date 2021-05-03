60ff70017647aa651b26b38b2162c27bad2c92bb
Matej Lajh <matej.lajh@outlook.com>
2021-05-03 19:30:38 +0200

    implemented prism example

--- RgPopupTest/RgPopupTest/App.xaml.cs
+++ RgPopupTest/RgPopupTest/App.xaml.cs
@@ -1,28 +1,37 @@
-﻿using System;
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Plugin.Popups;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace RgPopupTest
 {
-    public partial class App : Application
+    public partial class App
     {
-        public App()
+        /*
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor.
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
         }
 
-        protected override void OnStart()
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
-        }
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterPopupNavigationService();
 
-        protected override void OnSleep()
-        {
-        }
-
-        protected override void OnResume()
-        {
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<View1, View1ViewModel>();
         }
     }
 }
---
+++ RgPopupTest/RgPopupTest/MainPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace RgPopupTest
+{
+    public class MainPageViewModel : BindableBase
+    {
+        public string Test { get; set; } = "Hello from View Model";
+
+        private DelegateCommand _launchPopupCommand;
+        public DelegateCommand LaunchPopupCommand => _launchPopupCommand ?? (_launchPopupCommand = new DelegateCommand(ExecuteLaunchPopupCommand));
+        async void ExecuteLaunchPopupCommand()
+        {
+            await _navigationService.NavigateAsync("View1");
+        }
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        private INavigationService _navigationService;
+    }
+}
