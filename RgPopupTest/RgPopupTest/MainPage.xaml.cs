b9beba4f7786b168f2059ade812ee97f5036f8a7
Matej Lajh <matej.lajh@outlook.com>
2021-05-03 19:11:26 +0200

    init commit

---
+++ RgPopupTest/RgPopupTest/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RgPopupTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var popup = new View1();
+            await PopupNavigation.Instance.PushAsync(popup);
+        }
+    }
+}
